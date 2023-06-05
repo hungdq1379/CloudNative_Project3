5fa26699e2a1bbb15ed79b3dd310acb6118ec691
hungdq1379 <113217676+hungdq1379@users.noreply.github.com>
2023-06-05 21:43:56 +0700

    Update app.py

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def homepage():
     return render_template("main.html")
 
+# Register endpoint that returns 4xx error
+@app.route("/client-error")
+@metrics.summary('requests_by_status_4xx', 'Status Code', labels={
+    'code': lambda r: '400'
+})
+def client_error():
+    return "4xx Error", 400
+
+# Register endpoint that returns 5xx error
+@app.route("/server-error")
+@metrics.summary('requests_by_status_5xx', 'Status Code', labels={
+    'code': lambda r: '500'
+})
+def server_error():
+    return "5xx Error", 500
 
 if __name__ == "__main__":
     app.run()
