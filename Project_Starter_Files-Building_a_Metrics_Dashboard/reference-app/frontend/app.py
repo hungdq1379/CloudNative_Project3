2673f09b961fc0776d75e35754288a74dbcd0205
hungdq1379 <113217676+hungdq1379@users.noreply.github.com>
2023-06-06 21:56:01 +0700

    Update app.py

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,10 +1,72 @@
+import logging
+
 from flask import Flask, render_template, request
+from flask_opentracing import FlaskTracing
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter, Summary, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+# Create a metric to track time spent and requests made.
+s = Summary('request_processing_seconds', 'Time spent processing request')
+c = Counter('my_failures', 'Description of counter')
+
 
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+metrics = PrometheusMetrics(app)
+# static information as metric
+metrics.info("app_info", "Application info", version="1.0.3")
+
+logging.getLogger("").handlers = []
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Add prometheus wsgi middleware to route /metrics requests
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
+# register additional default metrics
+metrics.register_default(
+    metrics.counter(
+        'request_by_path_counter', 'Request count by request path',
+        labels={'path': lambda: request.path}
+    )
+)
+
+def init_tracer(service):
+
+    config = Config(
+        config={
+            "sampler": {"type": "const", "param": 1},
+            "logging": True,
+            "reporter_batch_size": 1,
+        },
+        service_name=service,
+        validate=True,
+        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer = init_tracer("frontend")
+flask_tracer = FlaskTracing(tracer, True, app)
 
 
+# Decorate function with metric.
+@s.time()
 @app.route("/")
 def homepage():
+    with tracer.start_span('homepage') as span:
+        span.set_tag('message', "Homepage")
+    
     return render_template("main.html")
 
 # Register endpoint that returns 4xx error
@@ -22,6 +84,7 @@ def client_error():
 })
 def server_error():
     return "5xx Error", 500
+
 
 if __name__ == "__main__":
     app.run()
